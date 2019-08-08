f6c2776ac69091ee43a313fb6133e86ddb20890e
paul <yummta@gmail.com>
2019-08-07 19:11:13 -0500

    gaa

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/** @jsx jsx */
+import { jsx, Global, css } from "@emotion/core";
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+// import { jsx, Global, css } from "@emotion/core";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div css={{}}>
+      <Global
+        styles={css`
+          body {
+            font-family: "Rubik", sans-serif;
+            line-height: 1.25;
+            margin: 0;
+            width: 100vw;
+            height: 100vh;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+          }
+        `}
+      />
+
+      <h1 css={{}}>Gaaaaaa</h1>
     </div>
   );
 }
